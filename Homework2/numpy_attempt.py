97e011fe20cceb6257568effab6bf41ee34ca7e0
maxb998 <boldrinmassimo98@gmail.com>
2022-05-01 15:37:48 +0200

    a corente

--- Homework2/numpy_attempt.py
+++ Homework2/numpy_attempt.py
@@ -1,7 +1,24 @@
+from array import array
 import sys
 import os
-import csv
 import numpy as np
+
+def findPerfectOutliers(points: np.array, num_of_outiliers: int) -> np.array:
+    refined_points = points.copy()
+    # create matrix with all distance between all points(for each point is calculated the distance between all other points)
+    n, dims = points.shape[0], points.shape[1]
+    points_dist = np.zeros(shape=(n, n), dtype=points.dtype)
+    onesMat = np.full(shape=())
+    for i in range(n):
+        
+        points_dist[i] = points - points[i]
+    
+    # now to compute the average distance of each point between all others
+    avgDist = np.full(shape=n, fill_value=1., dtype=points.dtype)
+    for i in range(n):
+
+
+    return refined_points
 
 def main():
 
@@ -26,8 +43,10 @@ def main():
     Z = int(Z)
     assert Z >= 0, "K must be positive"
 
+
     # Find outliers
     if Z > 0:
+        a = 0
         
     
     
