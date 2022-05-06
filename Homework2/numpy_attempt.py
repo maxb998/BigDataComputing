e02ba82c88968d75aa40a80a0a389a5f417f1e6a
maxb998 <boldrinmassimo98@gmail.com>
2022-05-06 16:57:32 +0200

    Final numpy
    To be commented

--- Homework2/numpy_attempt.py
+++ Homework2/numpy_attempt.py
@@ -2,10 +2,9 @@ import sys
 import os
 import time
 import numpy as np
-import matplotlib.pyplot as plt
 
 
-def calculate_r(points: np.ndarray, n: int) -> np.float32:
+def calculate_r(points: np.ndarray, n: int) -> np.float64:
     n = n-1
     reduced_points: np.ndarray = points[:n]
     distances: np.ndarray = np.zeros(shape=(n, n), dtype=points.dtype)
@@ -14,13 +13,10 @@ def calculate_r(points: np.ndarray, n: int) -> np.float32:
         distances[i] = np.linalg.norm(x=(reduced_points-reduced_points[i]), axis=-1)
     distances = distances[~np.eye(n,dtype=bool)].reshape(n,-1)  # remove diagonal which is all zeros
 
-    #print(distances)
-    #print(distances.min())
-
     return (distances.min() / 2.)
 
 
-def SeqWeightedOutliers(P: np.ndarray, W: np.ndarray, k: int, z: int, alpha: np.float32) -> tuple[np.ndarray, int, np.float32, np.float32]:
+def SeqWeightedOutliers(P: np.ndarray, W: np.ndarray, k: int, z: int, alpha: np.float64) -> tuple[np.ndarray, int, np.float64, np.float64]:
 
     n, dims = P.shape[0], P.shape[1]
     range_n: np.ndarray = np.arange(n)
@@ -33,49 +29,57 @@ def SeqWeightedOutliers(P: np.ndarray, W: np.ndarray, k: int, z: int, alpha: np.
         all_dist_squared[i] = np.sum(a=coord_diff_squared, axis=1, dtype=P.dtype)
     #all_distances_squared = all_distances_squared[~np.eye(n,dtype=bool)].reshape(n,-1)  # remove diagonal which is all zeros
 
-    r: np.float32 = calculate_r(P, k+z+1)
-    first_r: np.float32 = np.sqrt(r)
+    r: np.float64 = calculate_r(P, k+z+1)
+    first_r: np.float64 = r
 
     while True:
         S: np.ndarray = np.zeros(shape=(k, dims), dtype=P.dtype)
         is_uncovered: np.ndarray = np.ones(shape=n, dtype=np.bool8)
-        ball_radius_squared: np.float32 = ((1.+2.*alpha)*r)**2 # used when selecting the center
-        '''
-        print(all_dist_squared[0])
-        print(all_dist_squared[:,0])
-        print()
-        '''
+        ball_radius_squared: np.float64 = ((1.+2.*alpha)*r)**2 # used when selecting the center
+
         for i in range(k):
-            best_weight: np.float32 = 0.
+            best_weight: np.float64 = 0.
             best_pt_id: int = 0
 
             for current_pt in range(n):
                 if is_uncovered[current_pt]:
-                    current_weight = np.sum(a=W, dtype=np.float32, where=all_dist_squared[current_pt] < ball_radius_squared)
+                    current_weight = np.sum(a=W, dtype=np.float64, where=all_dist_squared[current_pt] < ball_radius_squared)
                     if current_weight > best_weight:
                         best_weight = current_weight
                         best_pt_id = current_pt
-            '''
-            print(best_pt_id)
-            print(best_weight)
-            print()
-            '''
+
             S[i] = P[best_pt_id]    # add new center
-            ball_radius_squared: np.float32 = ((3.+4.*alpha)*r)**2 #used when removing new covered points
+            ball_radius_squared: np.float64 = ((3.+4.*alpha)*r)**2 #used when removing new covered points
             is_uncovered[all_dist_squared[best_pt_id] < ball_radius_squared] = False
-            #print(is_uncovered.shape)
-            #print((all_dist_squared[best_pt_id] < ball_radius_squared))
 
         outliers_w = np.sum(a=W, where=is_uncovered)
-        #print(outliers_w)
-        #print(is_uncovered == 1)
         attempts += 1
         if outliers_w <= z:
-            return S, attempts, first_r, np.sqrt(r)
+            return S, attempts, first_r, r
         else:
             r = r * 2.
 
+def ComputeObjective(inputPoints: np.ndarray, solution: np.ndarray, weights: np.ndarray, z: int) -> np.float64 :
+    n, dims, k = inputPoints.shape[0], inputPoints.shape[1], solution.shape[0]
+    dist_from_centers: np.ndarray = np.zeros(shape=(n,k), dtype=inputPoints.dtype)
 
+    for i in range(n):
+        coord_diff_squared: np.ndarray = np.square(inputPoints[i] - solution)
+        dist_from_centers[i] = np.sum(a=coord_diff_squared, axis=1, dtype=inputPoints.dtype)
+    
+    min_dist_from_centers: np.ndarray = dist_from_centers.min(axis=1)
+    sorted_indexs: np.ndarray = np.argsort(a=min_dist_from_centers)
+
+    pos: int = -1
+    weight_sum: int = 0
+    for i in range(n-1, 0, -1):
+        weight_sum += weights[sorted_indexs[i]]
+        if weight_sum > z:
+            pos: int = sorted_indexs[i]
+            break
+            
+
+    return np.sqrt(min_dist_from_centers[pos])
 
 def main():
     np.set_printoptions(precision=2, linewidth=200)
@@ -87,7 +91,7 @@ def main():
     assert os.path.isfile(filename), "File or folder not found"
     try:
         f = open(filename)
-        data: np.ndarray = np.loadtxt(fname=filename, dtype=np.float32, delimiter=',')
+        data: np.ndarray = np.loadtxt(fname=filename, dtype=np.float64, delimiter=',')
     finally:
         f.close()
     
@@ -105,7 +109,7 @@ def main():
     weights: np.ndarray = np.ones(shape=data.shape[0], dtype=np.int64)
 
     # alpha
-    alpha: np.float32 = 0.
+    alpha: np.float64 = 0.
 
     # do k-center with weight and outliers
     millis_start: float = time.time() * 1000.
@@ -115,6 +119,9 @@ def main():
     # calculate time needed in milliseconds
     millis_duration: float = millis_end - millis_start
 
+    # Compute objective
+    obj: np.float64 = ComputeObjective(inputPoints=data, solution=centers, weights=weights, z=z)
+
     # print output
     print("Input size n = ", data.shape[0])
     print("Number of centers k = ", k)
@@ -122,20 +129,8 @@ def main():
     print("Initial guess = ", first_guess)
     print("Final guess = ", final_guess)
     print("Number of guesses = ", attemps)
-    print("Objective function = ")
+    print("Objective function = ", obj)
     print("Time of SeqWeightedOutliers = ", millis_duration)
-    print(centers)
-
-    plt1 = plt.figure(1)
-    plt.scatter(x=data[:,0], y=data[:,1], marker='.')
-
-    plt2 = plt.figure(2)
-    plt.scatter(x=centers[:,0], y=centers[:,1], marker='.')
-
-    plt.show()
-
-
-
 
 
 if __name__ == "__main__":
