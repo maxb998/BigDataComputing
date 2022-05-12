bf0936839b5f50d2eee97c432a2ab092e139281c
123Sanpei <102912330+123Sanpei@users.noreply.github.com>
2022-05-13 00:32:42 +0200

    Update G001HW2.py

--- Homework2/G001HW2.py
+++ Homework2/G001HW2.py
@@ -2,7 +2,7 @@ import time
 import sys
 import math
 import os
-
+import numpy as np
 
 def readVectorsSeq(filename):
     with open(filename) as f:
@@ -11,32 +11,33 @@ def readVectorsSeq(filename):
     
     
 def euclidean(point1,point2):
-    res = 0
-    for i in range(len(point1)):
-        diff = (point1[i]-point2[i])
-        res +=  diff*diff
-    return math.sqrt(res)
+    res = np.linalg.norm(point1-point2, axis=1)
+    return res
 
 def Bz(x,r,Z):
-    result = []
-    for y in Z:
-        if euclidean(x,y)<=r:
-            result.append(y)
-    return result
+    #result = []
+    X = np.array(x)
+    idx = np.where(euclidean(X,Z) <= r)
+    #ball_points = Y[idx]
+    #result = list(map(tuple, ball_points))
+    
+    return idx
 
 def SeqWeightedOutliers(P,W,k,z,alpha):
     # find r which is the minimum half distance between the first k+z+1 points
-    r_min = []
+    P_kz1 = np.array(P[:k+z+1]) # We take only the first k+z+1 points from the pointset P
+    r_min = np.zeros(shape=(k+z+1,k+z+1))
     for i in range(k+z+1):
-        for j in range(k+z+1):
-            if i!=j:
-                r_min.append(euclidean(P[i], P[j]))
+        r_min[i] = euclidean(P_kz1,P_kz1[i])
+        r_min[i,i] = np.max(r_min[i]) # We assign the maximum value of the row to avoid considering the diagonal(which is all zeros)
 
-    r = min(r_min)/2
+    r = np.min(r_min)/2
+    
     # r_i is the initial guess
     r_i = r
     # n_guess is the number of cycles
     n_guess = 0
+    w = np.array(W)
     while(True):
         Z = P.copy()
         S = []
@@ -47,17 +48,15 @@ def SeqWeightedOutliers(P,W,k,z,alpha):
             
             max_v = 0
             for x in Z:
-                temp = 0
-                for j in Bz(x,(1+2*alpha)*r, Z):
-                    temp += W[P.index(j)] 
-                ball_weight = temp 
+                idx = Bz(x,(1+2*alpha)*r, Z) # Indices of the points inside the ball
+                ball_weight = np.sum(w[idx])
                 if ball_weight > max_v:
                     max_v = ball_weight
                     newcenter = x
-            S.append(newcenter)
-            for y in Bz(newcenter,(3+4*alpha)*r, Z):
-                Z.remove(y)
-                Wz = Wz - W[P.index(y)]
+            S.append(newcenter) # Add the new center found
+            idx = Bz(newcenter,(3+4*alpha)*r, Z)
+            Z = np.delete(Z,idx,0)
+            Wz = Wz - np.sum(w[idx])
         if Wz<=z:
             # r_f is the final guess
             r_f = r
@@ -68,26 +67,28 @@ def SeqWeightedOutliers(P,W,k,z,alpha):
                 
 def ComputeObjective(P,S,z):
 
-    min_dist_from_centers = []
-    dist_from_centers = [0.] * len(S)
-    
+    min_dist_from_centers = np.zeros(len(P))
+    dist_from_centers = np.zeros(shape=(len(P),len(S)))
+    points = np.array(P)
+    centers = np.array(S)
+
     for i in range(len(P)):
-        for j in range(len(S)):
-            dist_from_centers[j] = euclidean(P[i],S[j])
-        min_dist_from_centers.append(min(dist_from_centers)) # Adding the minimum distance of the single point x from the all the centers
-        min_dist_from_centers.sort() # Sorting at every cycle to reduce the computational time of the last sort since almost all element will be already sorted
-    min_dist_from_centers.sort()
-    return min_dist_from_centers[-(z+1)]
+        dist_from_centers[i] = euclidean(points[i],centers)
+
+    min_dist_from_centers = np.min(dist_from_centers, axis=1)
+    result = np.sort(min_dist_from_centers, axis=None)
+    return result[-(z+1)]
 
 def main():
     
     # Check argv lenght and content
-    assert len(sys.argv) == 4, "Usage: python G001HW2.py <Filename> <k> <z>"
+    assert len(sys.argv) == 4, "Usage: python prova.py testdataHW2.txt 3 3"
     
     filename = sys.argv[1]
     assert os.path.isfile(filename), "File or folder not found"
     # Read the points in the input file into a list of tuple
     inputPoints = readVectorsSeq(filename)
+    a = np.array(inputPoints)
     # Create a list of ones 'weights' of the same cardinality of inputPoints
     n = len(inputPoints)
     weights = [1]*n
@@ -108,7 +109,7 @@ def main():
     end = time.time() * 1000
     # Method that returns the largest distange among all distances(x,S), for x in P, excluding the first z largest disatnces
     objective =  ComputeObjective(inputPoints,solution,z)
-    
+
     # Output
     print("Input size n =", n)
     print("Number of centers k =", k)
@@ -118,9 +119,6 @@ def main():
     print("Number of guesses =", n_guess)
     print("Objective function =", objective)
     print("Time of SeqWeightedOutliers =", end-start)
-     
-            
-
 
 
 if __name__ == "__main__":
